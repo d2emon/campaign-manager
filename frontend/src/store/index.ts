fd2924a9b28cc115ebd820dd743f3b2003be0e5e
Dmitry Kutsenko <d2emonium@gmail.com>
2025-05-08 16:29:38 +0300

    Integrate quests and notes APIs into the store: add reducers and middleware for quests and notes to enhance state management and API interactions.

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -3,6 +3,8 @@ import authReducer from 'modules/auth/store/auth';
 import { campaignsApi } from 'modules/campaign/services/campaignApi';
 import { npcApi } from 'modules/character/services/npcApi';
 import { locationsApi } from 'modules/location/services/locationApi';
+import { questsApi } from 'modules/quest/services/questApi';
+import { notesApi } from 'modules/note/services/noteApi';
 
 export const store = configureStore({
   reducer: {
@@ -10,11 +12,14 @@ export const store = configureStore({
     [campaignsApi.reducerPath]: campaignsApi.reducer,
     [locationsApi.reducerPath]: locationsApi.reducer,
     [npcApi.reducerPath]: npcApi.reducer,
+    [notesApi.reducerPath]: notesApi.reducer,
+    [questsApi.reducerPath]: questsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     campaignsApi.middleware,
     npcApi.middleware,
     locationsApi.middleware,
+    questsApi.middleware,
   ),
 });
 
